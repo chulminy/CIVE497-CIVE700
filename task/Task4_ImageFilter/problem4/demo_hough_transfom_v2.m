1b92a36dc8e49f1c050e4605e40d773983e075b1
chulminy <cheolminy@gmail.com>
2019-03-01 13:43:43 -0500

    update

---
+++ task/Task4_ImageFilter/problem4/demo_hough_transfom_v2.m
@@ -0,0 +1,24 @@
+% Hough Transformation
+% Author: Chul Min Yeum (cmyeum@uwaterloo.ca)
+% Last update:: 02/28/19
+clear; close all; clc; format shortG;
+
+%% Parameter
+bookletSize = [23 30]; % cm
+bookletImgSize = bookletSize*50; % output image size
+dirImg = 'img'; % image folder
+dirOut = 'out'; % output image folder
+imgList = dir('img/*.JPG'); 
+nImg = numel(imgList);
+
+%% Processing
+for ii=1:nImg
+    img = imread(fullfile(dirImg, imgList(ii).name));
+    corner = FindCorner(img); % find ordered four corners
+   
+    H = ComputeH(corner, bookletImgSize); % compute a homography
+    
+    [imgTran, RA] = imwarp(img, projective2d(inv(H)));
+    bookletImg = imcrop(imgTran, [-RA.XWorldLimits(1), -RA.YWorldLimits(1) bookletImgSize]);
+    imwrite(bookletImg, fullfile(dirOut,imgList(ii).name));
+end
